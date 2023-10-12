99ccdbcb43a6270e055ce49f1cf20490907e82b5
Daniel Andrews <daniel.korienko@gmail.com>
2023-10-12 11:02:33 +0300

    Added raw input toggle feature

--- Pizza-Delivery-Game/Assets/Code/Scripts/Misc/KeyboardInput.cs
+++ Pizza-Delivery-Game/Assets/Code/Scripts/Misc/KeyboardInput.cs
@@ -5,19 +5,39 @@ namespace Misc
 {
     public class KeyboardInput : MonoBehaviour
     {
+        [Header("External references")]
         [SerializeField] private CharacterControllerMovement _movement;
 
+        [Header("Settings")] 
+        [SerializeField] private bool _useRawInput;
+
+        private float _horizontal;
+        private float _vertical;
+        
         private void Update()
         {
-            float horizontal = Input.GetAxis(Axis.Horizontal);
-            float vertical = Input.GetAxis(Axis.Vertical);
-
-            var input = new Vector2(horizontal, vertical);
+            ReadInput();
+            
+            var input = new Vector2(_horizontal, _vertical);
             
             _movement.Move(input);
         }
 
         public static bool IsEKeyPressedDown()
             => Input.GetKeyDown(KeyCode.E);
+
+        private void ReadInput()
+        {
+            if (_useRawInput)
+            {
+                _horizontal = Input.GetAxisRaw(Axis.Horizontal);
+                _vertical = Input.GetAxisRaw(Axis.Vertical);
+            }
+            else
+            {
+                _horizontal = Input.GetAxis(Axis.Horizontal);
+                _vertical = Input.GetAxis(Axis.Vertical);
+            }
+        }
     }
 }
