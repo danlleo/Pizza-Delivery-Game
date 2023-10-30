e01785cbffc58c543f76fbc18ac6e7ad1cf63f12
Daniel Andrews <daniel.korienko@gmail.com>
2023-10-30 23:59:34 +0200

    Update
    Added UIAudio class, added new sound clip to play when player receives a new item!

---
+++ Pizza-Delivery-Game/Assets/Code/Scripts/Sounds/Audio/UIAudio.cs
@@ -0,0 +1,38 @@
+using System;
+using UI.InspectableObject;
+using UnityEngine;
+
+namespace Sounds.Audio
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class UIAudio : AudioPlayer
+    {
+        [Header("External references")] 
+        [SerializeField] private UI.UI _ui;
+        
+        [Header("Settings")]
+        [SerializeField] private AudioClip _itemObtained;
+
+        private AudioSource _audioSource;
+
+        private void Awake()
+        {
+            _audioSource = GetComponent<AudioSource>();
+        }
+
+        private void OnEnable()
+        {
+            _ui.InspectableObjectOpeningEvent.Event += InspectableObjectOpening_Event;
+        }
+
+        private void OnDisable()
+        {
+            _ui.InspectableObjectOpeningEvent.Event -= InspectableObjectOpening_Event;
+        }
+
+        private void InspectableObjectOpening_Event(object sender, InspectableObjectOpeningEventArgs e)
+        {
+            PlaySound(_audioSource, _itemObtained, 0.625f);
+        }
+    }
+}
