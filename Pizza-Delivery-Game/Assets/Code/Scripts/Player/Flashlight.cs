b51894c9d53d394098274509fb7aa7ff52526222
Daniel Andrews <daniel.korienko@gmail.com>
2023-11-02 23:15:55 +0200

    Update
    Flashlight logic updated, visuals as well

--- Pizza-Delivery-Game/Assets/Code/Scripts/Player/Flashlight.cs
+++ Pizza-Delivery-Game/Assets/Code/Scripts/Player/Flashlight.cs
@@ -1,3 +1,5 @@
+using System;
+using DG.Tweening;
 using Sounds.Audio;
 using UnityEngine;
 
@@ -7,12 +9,39 @@ namespace Player
     public class Flashlight : MonoBehaviour
     {
         [Header("External references")] 
+        [SerializeField] private Transform _flashLightHolderTransform;
+        [SerializeField] private Camera _camera;
         [SerializeField] private PlayerAudio _playerAudio;
         [SerializeField] private Light _lightSource;
 
-        [Header("Settings")]
+        [Header("Settings")] 
+        [SerializeField] private float _followDuration = 0.75f;
         [SerializeField] private bool _isEnabled = true;
-        [SerializeField] private bool _isOn;
+        
+        private bool _isOn;
+
+        private void Awake()
+        {
+            _lightSource.enabled = false;
+        }
+
+        private void Update()
+        {
+            if (!_isEnabled) return;
+            
+            PlaceAtHolderPosition();
+            FollowCameraWithSmooth();
+        }
+
+        private void PlaceAtHolderPosition()
+        {
+            _lightSource.transform.position = _flashLightHolderTransform.position;
+        }
+        
+        private void FollowCameraWithSmooth()
+        {
+            _lightSource.transform.DORotateQuaternion(_camera.transform.rotation, _followDuration);
+        }
         
         public void ToggleLight()
         {
