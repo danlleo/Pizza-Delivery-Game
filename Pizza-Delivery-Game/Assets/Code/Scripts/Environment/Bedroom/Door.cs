95bdd04d9665553398b8cc32cab3d1ffe71f12dd
Daniel Andrews <daniel.korienko@gmail.com>
2023-11-11 23:44:45 +0200

    Update
    Now you interact with a door only after player woke up

--- Pizza-Delivery-Game/Assets/Code/Scripts/Environment/Bedroom/Door.cs
+++ Pizza-Delivery-Game/Assets/Code/Scripts/Environment/Bedroom/Door.cs
@@ -1,3 +1,4 @@
+using System;
 using Enums.Scenes;
 using Interfaces;
 using Misc;
@@ -9,11 +10,31 @@ using UnityEngine;
 namespace Environment.Bedroom
 {
     [SelectionBase]
+    [RequireComponent(typeof(BoxCollider))]
     [DisallowMultipleComponent]
     public class Door : MonoBehaviour, IInteractable
     {
         [SerializeField] private BedroomAudio _bedroomAudio;
+
+        private BoxCollider _boxCollider;
+
+        private void Awake()
+        {
+            _boxCollider = GetComponent<BoxCollider>();
+            
+            DisableDoorCollider();
+        }
+
+        private void OnEnable()
+        {
+            WokeUpStaticEvent.OnWokeUp += WokeUpStaticEvent_OnWokeUp;
+        }
         
+        private void OnDisable()
+        {
+            WokeUpStaticEvent.OnWokeUp -= WokeUpStaticEvent_OnWokeUp;
+        }
+
         public void Interact()
         {
             Crossfade.Instance.FadeIn(InputAllowance.DisableInput, () => Loader.Load(Scene.TestingFeaturesScene));
@@ -24,6 +45,17 @@ namespace Environment.Bedroom
         public string GetActionDescription()
         {
             return "Leave";
+        }
+
+        private void EnableDoorCollider()
+            => _boxCollider.enabled = true;
+
+        private void DisableDoorCollider()
+            => _boxCollider.enabled = false;
+        
+        private void WokeUpStaticEvent_OnWokeUp(object sender, EventArgs e)
+        {
+            EnableDoorCollider();
         }
     }
 }
