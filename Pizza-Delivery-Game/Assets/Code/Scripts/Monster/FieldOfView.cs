d025a4020f49b34686b96ec7e99050b93d2a18d4
Daniel Andrews <daniel.korienko@gmail.com>
2023-11-18 23:05:05 +0200

    Update
    Added field of view for enemy (prototype)

---
+++ Pizza-Delivery-Game/Assets/Code/Scripts/Monster/FieldOfView.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using UnityEngine;
+
+namespace Monster
+{
+    public class FieldOfView : MonoBehaviour
+    {
+        [Header("Settings")]
+        [SerializeField] private LayerMask _targetMask;
+        [SerializeField] private LayerMask _obstructionLayerMask;
+        
+        [SerializeField] private float _radius;
+        [SerializeField, Range(0f, 360f)] private float _angle;
+
+        private GameObject _playerGameObject;
+        
+        private void Start()
+        {
+            _playerGameObject = Player.Player.Instance.gameObject;
+            StartCoroutine(FOVRoutine());
+        }
+
+        private IEnumerator FOVRoutine()
+        {
+            var wait = new WaitForSeconds(0.2f);
+            
+            while (true)
+            {
+                yield return wait;
+                CheckFieldOfView();
+            }
+        }
+
+        private void CheckFieldOfView()
+        {
+            Collider[] rangeCheck = Physics.OverlapSphere(transform.position, _radius, _targetMask);
+
+            if (rangeCheck.Length <= 0) return;
+            
+            Transform target = rangeCheck[0].transform;
+            Vector3 directionToTarget = (target.position - transform.position).normalized;
+
+            // We divide angle by two because we have to check both sides from transform forward: \./
+            if (Vector3.Angle(transform.forward, directionToTarget) < _angle / 2)
+            {
+                // If within eyesight 
+                float distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+                if (!Physics.Raycast(transform.position, directionToTarget, distanceToTarget, _obstructionLayerMask))
+                {
+                    // If we detect target with our raycast
+                }
+            }
+        }
+    }
+}
