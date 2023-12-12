4025a7d325c9a3797345d6cc96fad9a7d0e8a914
Daniel Andrews <daniel.korienko@gmail.com>
2023-12-12 22:57:21 +0200

    Update
    Added useful extensions by @thofnas

---
+++ Pizza-Delivery-Game/Assets/Code/Scripts/Misc/ListExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utilities
+{
+    public static class ListExtensions
+    {
+        public static void Log<T>(this List<T> messages)
+        {
+            foreach (T message in messages)
+                Debug.Log(message);
+        }
+    }
+}
