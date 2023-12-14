14f03ca7ddc7880034392e8db8ba2a5502d5a8a2
Daniel Andrews <daniel.korienko@gmail.com>
2023-12-14 22:57:37 +0200

    Cleanup

--- Pizza-Delivery-Game/Assets/Code/Scripts/Misc/Axis.cs
+++ Pizza-Delivery-Game/Assets/Code/Scripts/Misc/Axis.cs
@@ -2,8 +2,6 @@ namespace Misc
 {
     public static class Axis
     {
-        public static readonly string Horizontal = "Horizontal";
-        public static readonly string Vertical = "Vertical";
         public static readonly string MouseX = "Mouse X";
         public static readonly string MouseY = "Mouse Y";
     }
